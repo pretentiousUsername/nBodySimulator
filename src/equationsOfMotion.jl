3150bb867566cd871442fc682ba7de28908b54af
pretentiousUsername <mitchell.ian.2001@gmail.com>
2023-07-19 09:04:45 -0400

    not what I intended Somehow whenever I implement an RK4 solver in this program it just kinda craps out. I need to find out why.

--- src/equationsOfMotion.jl
+++ src/equationsOfMotion.jl
@@ -33,3 +33,32 @@ function positionStep(v::Vector{Float64}, q::Particle, dt::Float64)
     θⱼ₊₁ = @. θⱼ + v * dt
     return θⱼ₊₁
 end
+
+# This function might create some errors since not all particle positions are
+# getting updated at once.
+function rungeKuttaStep(q::Particle, list::Vector{Particle}, box::Container, dt::Float64)
+    ak₁ = accelerationStep(q, list, box)
+    vk₁ = velocityStep(ak₁, q, dt)
+    θk₁ = positionStep(vk₁, q, dt)
+    qk₁ = Particle(θk₁, vk₁, ak₁, q.mass, q.label) # a bit hacky, but it should
+                                                   # hopefully work.
+    ak₂ = accelerationStep(qk₁, list, box)
+    vk₂ = velocityStep(ak₂, qk₁, dt / 2)
+    θk₂ = positionStep(vk₂, qk₁, dt / 2)
+    qk₂ = Particle(θk₂, vk₂, ak₂, q.mass, q.label)
+
+    ak₃ = accelerationStep(qk₂, list, box)
+    vk₃ = velocityStep(ak₃, qk₂, dt / 2)
+    θk₃ = positionStep(vk₃, qk₂, dt / 2)
+    qk₃ = Particle(θk₂, vk₂, ak₂, q.mass, q.label)
+
+    ak₄ = accelerationStep(qk₃, list, box)
+    vk₄ = velocityStep(ak₄, qk₃, dt)
+    θk₄ = positionStep(vk₄, qk₃, dt)
+
+    a = q.acceleration + (ak₁ + 2ak₂ + 2ak₃ + ak₄) * dt / 6
+    v = q.velocity + (vk₁ + 2vk₂ + 2vk₃ + vk₄) * dt / 6
+    θ = q.position + (θk₁ + 2θk₂ + 2θk₃ + θk₄) * dt / 6
+
+    return [θ, v, a]
+end
